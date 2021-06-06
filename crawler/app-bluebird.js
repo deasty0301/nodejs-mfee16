8c50c1c08a335ee33cf011fe9b82062ced1fa586
deasty0301 <83852383+deasty0301@users.noreply.github.com>
2021-06-06 15:46:26 +0800

    Add files via upload

---
+++ crawler/app-bluebird.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const Promise = require("bluebird");
+
+console.log(moment().format()); // 2021-05-30T13:45:06+08:00
+console.log(moment().format("YYYYMMDD")); // 20210530
+
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+// use bluebird
+const readFileBlue = Promise.promisify(fs.readFile);
+
+readFileBlue("stock.txt", "utf8")
+  .then((stockCode) => {
+    console.log("stockCode:", stockCode);
+
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
+  })
+  .then((response) => {
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ crawler/fs-promise-sql.js
@@ -0,0 +1,46 @@
+//引入各個需求
+const axios = require("axios");
+const fs = require("fs/promises");
+const moment = require("moment");
+const mysql = require("mysql");
+const Promise = require("bluebird");
+//引入 dotenv
+require("dotenv").config();
+
+//建立連線所需資料
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+//建立連線
+connection = Promise.promisifyAll(connection);
+
+(async function () {
+  try {
+    await connection.connectAsync();
+
+    
+    let stockCode = await fs.readFile("stock.txt", "utf-8");
+    console.log(`我的 stock code: ${stockCode}`);
+    let stock = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = ${stockCode}`);
+
+    if (stock.length <= 0) {
+        let response = await axios.get(`https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
+    );
+    let answer = response.data.suggestions.shift();
+    let answers = answer.split("\t");
+        // console.log(stockInfos);
+        if (answers.length > 1) {
+            //TODO: answers[0], answers[1]
+            connection.queryAsync(`INSERT INTO stock (stock_id, stock_name) VALUES ('${answers[0]}', '${answers[1]}')`);
+        }
+    }
+} catch (err) {
+    console.error (err);
+} finally {
+    connection.end();
+}
+})();
